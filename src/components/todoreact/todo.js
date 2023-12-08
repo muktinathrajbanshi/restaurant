77740483fe8cd71ab770129e86416dc24e120c5e
unknown <muktinathrajbanshi786@gmail.com>
2023-12-08 15:53:51 +0545

    adding items in todo list

--- src/components/todoreact/todo.js
+++ src/components/todoreact/todo.js
@@ -1,22 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react';
 import "./style.css";
 const Todo = () => {
+    const [inputdata, setInputData] = useState("");
+    const [items, setItems] = useState([]);
+    // add the items function
+    const addItem = () => {
+        if(!inputdata) {
+            alert('plz fill the data');
+        } else {
+            setItems([... items, inputdata])
+        }   setInputData("");
+    };
   return (
     <>
         <div className='main-div'>
             <div className='child-div'>
                 <figure>
                     <img src="./images/todo.svg" alt="todologo" />
-                    <figcaption>Add Your List Here ❤️</figcaption>
+                    <figcaption>Add Your List Here 👌</figcaption>
                 </figure>
                     <div className='addItems'>
                         <input type="text"
                         placeholder="✍️ Add Item" 
                         className="form-control"
+                        value={ inputdata }
+                        onChange={(event) => setInputData(event.target.value)}
                         />
-                        <i className="fa-solid fa-plus"></i>
+                        <i className="fa fa-plus add-btn" onClick={addItem}></i>
                     </div>
-                        <div className='showItem'><button className='btn effect04'></button> </div>
+                    {/* show our items */}
+                        <div className='showItems'>
+                            {items.map((curElem, index) => {
+                               return ( 
+                                <div className='eachItem' key={index} >
+                                <h3>{ curElem }</h3>
+                                <div className='todo-btn'>
+                                <i className="far fa-edit add-btn"></i>
+                                <i className="far fa-trash-alt add-btn"></i>
+                                </div>
+                                </div>
+                               );  
+                            })}
+
+                            
+                        </div>
+                    {/* remove all button */}
+                        <div className='showItems'>
+                            <button className='btn effect04' data-sm-link-text="Remove All">
+                                <span>CHECK LIST</span>
+                            </button> 
+                        </div>
             </div>
         </div>
     </>
